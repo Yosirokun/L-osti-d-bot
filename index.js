8e072c1e5b5bf9cfb007d7492c91ff2845d06c1e
Sedrick <sedrickidd@hotmail.com>
2019-04-16 18:07:21 -0400

    Base bot working

---
+++ index.js
@@ -0,0 +1,13 @@
+const discord = require('discord.js');
+const bot = new discord.Client();
+
+
+
+bot.on('ready', function()
+{
+    console.log("Le bot run!");
+});
+
+
+
+bot.login('NTY3ODI5NDU3NzEzNDk2MDc1.XLZR5g.7cL1zxii7vvIoCz-uV-1hn2gtXY')
