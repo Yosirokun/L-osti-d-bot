4b040dc034c5b2ec481b2a2a0f928c664be87622
yosiro <sedrickidd@hotmail.com>
2019-04-28 18:00:23 -0400

    Music is working finaly

--- commands/music/YTPlayer.js
+++ commands/music/YTPlayer.js
@@ -1,36 +1,26 @@
 const commando = require('discord.js-commando');
 const ytdl = require('ytdl-core');
 
-
-class YTPlayer
+function play (connection, message, server)
 {
-    constructor()
-    {
-        this.isPlaying = false;     
-    }
-    
-    play(connection, message, server)
-    {
-        this.isPlaying = true;
-        
-        
-            console.log("in play");
-            server.dispatcher = connection.playStream(ytdl(server.queue[0], { audioonly: true }), { passes : 5 });
-            server.queue.shift();
-            server.dispatcher.on("start", function()
+    server.dispatcher = connection.playStream(ytdl(server.queue[0], { audioonly: true }), { passes : 5 });
+    setEvents(connection, message, server);
+    server.queue.shift();
+}
+function setEvents(connection, message, server)
+{
+    server.dispatcher.on("start", function()
             {
                 console.log("start event");
             });
-            server.dispatcher.on("end", function()
-            {
-                
+            server.dispatcher.on("end",async function()
+            {               
                 this.isPlaying = false;
                 console.log("in the end event" + this.isPlaying);
                 if(server.queue[0])
                 {
                     console.log("should play again");
-                    this.play(connection, message, server);
-                    
+                    await play(connection, message, server);     
                 }
                 else
                 {
@@ -38,28 +28,44 @@ class YTPlayer
                     message.guild.voiceConnection.disconnect();                          
                 }
             });
-        }
-      
+}
+class YTPlayer
+{
+    constructor()
+    {
+        this.isPlaying = false;     
+    }
     
-
-
+    async play(connection, message, server)
+    {
+        try{
+            this.isPlaying = true;         
+            console.log("in play");
+            play(connection, message, server);                     
+        }
+        catch(err){console.log("an error as occured" + err);}
+    }
+    
+    
+    
+    
     stopPlaying(server)
     {
         server.queue = [];
         this.isPlaying = false;
         console.log("stopped playing");
     }
-
+    
     getIsPlaying()
     {
         return this.isPlaying;
     }
-
+    
     setIsPlaying(value)
     {
         this.isPlaying = value;
     }
-
+    
 }
 
 
